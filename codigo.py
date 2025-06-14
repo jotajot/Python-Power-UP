c0beb7d55d44dd7506f56ad0bab01a47a6cad993
José João <josejoaomonteirocosta8@gmail.com>
2025-06-14 12:00:55 -0300

    função com matriz das coordenadas do cursor e aba de seleção das lojas

--- codigo.py
+++ codigo.py
@@ -9,21 +9,36 @@ import pygetwindow as gw
 
 pyautogui.PAUSE = 1
 
-# Passo 1: Entrar no sistema da empresa - https://dlp.hashtagtreinamentos.com/python/intensivao/login
+def coordenadas() : 
+    return [
+        [900, 580],
+        [350, 72],
+        [273, 383],
+        [273, 510],
+        [500, 286],
+        [500, 338],
+        [500, 358]
+]
+
+coordenada = coordenadas()
+
+
+# Passo 1: Entrar no chrome
 # abrir o chrome
 pyautogui.press("win")
 pyautogui.write("chrome")
 pyautogui.press("Enter")
-pyautogui.moveTo(x = 900, y = 580)
-pyautogui.click(x = 900, y = 580)
-pyautogui.click(button = 'left')
+pyautogui.moveTo(coordenada[0])
+pyautogui.click(coordenada[0])
+#pyautogui.click(button = 'left')
 
 time.sleep(1)
 
-pyautogui.moveTo(x = 350, y = 100)
-pyautogui.click(x = 350, y = 72)
+#pyautogui.moveTo(coordenada[1])
+#pyautogui.click(coordenada[1])
 #time.sleep(1)
 
+# Passo 2: Fazer login
 # digitar o site
 pyautogui.write("https://app.swfast.com.br")
 pyautogui.press("Enter")
@@ -33,12 +48,23 @@ pyautogui.press("Tab")
 pyautogui.write("Josejoaojj_17")
 pyautogui.press("Enter")
 time.sleep(3)
-pyautogui.moveTo(x = 273, y=383)
-pyautogui.click(x=273, y=383)
-pyautogui.moveTo(x=273, y=510)
-pyautogui.click(button="left")
 
-# Passo 2: Fazer login
-# Passo 3: Importar a base de dados
+# Passo 3: Lozalizar relatório de faturamento por tipo de pagamento
+# Localiza o relatório descritivo 
+pyautogui.moveTo(coordenada[2])
+pyautogui.click(coordenada[2])
+pyautogui.moveTo(coordenada[3])
+pyautogui.click(button="left")
+pyautogui.moveTo(coordenada[4])
+time.sleep(1)
+pyautogui.click(coordenada[4])
+pyautogui.write("faturamento por tipo de pagamento")
+pyautogui.press("Enter")
+pyautogui.click(coordenada[5])
+pyautogui.moveTo(coordenada[6])
+pyautogui.click(coordenada[6])
+
+
+
 # Passo 4: Cadastrar 1 produto
 # Passo 5: Cadastrar o restante dos produtos
